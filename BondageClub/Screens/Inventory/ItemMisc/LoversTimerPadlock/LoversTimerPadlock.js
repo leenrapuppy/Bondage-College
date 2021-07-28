66ccdfdece20c43c3cd0715c0d9f5e1cc965388e
ace-1331 <59637973+ace-1331@users.noreply.github.com>
2021-07-28 13:56:49 -0400

    fix timer lover lock (#2804)

--- BondageClub/Screens/Inventory/ItemMisc/LoversTimerPadlock/LoversTimerPadlock.js
+++ BondageClub/Screens/Inventory/ItemMisc/LoversTimerPadlock/LoversTimerPadlock.js
@@ -23,7 +23,7 @@ function InventoryItemMiscLoversTimerPadlockDraw() {
     DrawText(DialogFindPlayer(DialogFocusItem.Asset.Group.Name + DialogFocusItem.Asset.Name + "Intro"), 1500, 600, "white", "gray");
 
     // Draw the settings
-    if (Player.CanInteract() && (C.IsLoverOfPlayer())) {
+    if (Player.CanInteract() && (C.IsLoverOfPlayer() || C.IsOwnedByPlayer())) {
         MainCanvas.textAlign = "left";
         DrawButton(1100, 666, 64, 64, "", "White", (DialogFocusSourceItem.Property.RemoveItem) ? "Icons/Checked.png" : "");
         DrawText(DialogFindPlayer("RemoveItemWithTimer"), 1200, 698, "white", "gray");
@@ -40,7 +40,7 @@ function InventoryItemMiscLoversTimerPadlockDraw() {
     }
 
     // Draw buttons to add/remove time if available
-    if (Player.CanInteract() && (C.IsLoverOfPlayer())) {
+    if (Player.CanInteract() && (C.IsLoverOfPlayer() || C.IsOwnedByPlayer())) {
         DrawButton(1100, 910, 250, 70, DialogFindPlayer("AddTimerTime"), "White");
         DrawBackNextButton(1400, 910, 250, 70, LoverTimerChooseList[LoverTimerChooseIndex] + " " + DialogFindPlayer("Hours"), "White", "",
             () => LoverTimerChooseList[(LoverTimerChooseList.length + LoverTimerChooseIndex - 1) % LoverTimerChooseList.length] + " " + DialogFindPlayer("Hours"),
@@ -60,9 +60,9 @@ function InventoryItemMiscLoversTimerPadlockDraw() {
 function InventoryItemMiscLoversTimerPadlockClick() {
     if ((MouseX >= 1885) && (MouseX <= 1975) && (MouseY >= 25) && (MouseY <= 110)) InventoryItemMiscLoversTimerPadlockExit();
     if (!Player.CanInteract()) return;
-    var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
+    var C = CharacterGetCurrent();
 
-    if (C.IsLoverOfPlayer()) {
+    if (C.IsLoverOfPlayer() || C.IsOwnedByPlayer()) {
         if ((MouseX >= 1100) && (MouseX <= 1164)) {
             if ((MouseY >= 666) && (MouseY <= 730)) { DialogFocusSourceItem.Property.RemoveItem = !(DialogFocusSourceItem.Property.RemoveItem); }
             if ((MouseY >= 746) && (MouseY <= 810)) { DialogFocusSourceItem.Property.ShowTimer = !(DialogFocusSourceItem.Property.ShowTimer); }
@@ -72,7 +72,7 @@ function InventoryItemMiscLoversTimerPadlockClick() {
     }
 
     if ((MouseY >= 910) && (MouseY <= 975)) {
-        if (C.IsLoverOfPlayer()) {
+        if (C.IsLoverOfPlayer() || C.IsOwnedByPlayer()) {
             if ((MouseX >= 1100) && (MouseX < 1350)) InventoryItemMiscLoversTimerPadlockAdd(LoverTimerChooseList[LoverTimerChooseIndex] * 3600);
             if ((MouseX >= 1400) && (MouseX < 1650)) {
                 if (MouseX <= 1525) LoverTimerChooseIndex = (LoverTimerChooseList.length + LoverTimerChooseIndex - 1) % LoverTimerChooseList.length;
@@ -123,5 +123,5 @@ function InventoryItemMiscLoversTimerPadlockAdd(TimeToAdd, PlayerMemberNumberToL
 // Exits the extended menu
 function InventoryItemMiscLoversTimerPadlockExit() {
     DialogFocusItem = null;
-    if (DialogInventory != null) DialogMenuButtonBuild((Player.FocusGroup != null) ? Player : CurrentCharacter);
+    if (DialogInventory != null) DialogMenuButtonBuild(CharacterGetCurrent());
 }
