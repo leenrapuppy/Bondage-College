e9da70d306e4caac1f12f27258f79afda0bf5da9
Ben987 <30805159+Ben987@users.noreply.github.com>
2021-08-27 14:47:27 -0400

    Bondage Poker - Emily
    Bondage Poker - Emily

--- BondageClub/Screens/Room/Poker/Poker.js
+++ BondageClub/Screens/Room/Poker/Poker.js
@@ -29,7 +29,7 @@ var PokerAsset = [
 	{
 		Family: "Model",
 		Type: "Set",
-		Opponent: ["Andrea", "Akira", "Becky", "Dita"]
+		Opponent: ["Andrea", "Akira", "Becky", "Dita", "Emily"]
 	}
 ];
 var PokerPlayerCount = 4;
