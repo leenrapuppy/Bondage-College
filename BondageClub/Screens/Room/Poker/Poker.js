d5f524e237636cc060ef9d5e57a6f95a14f7cbb6
Ben987 <30805159+Ben987@users.noreply.github.com>
2021-08-30 17:16:12 -0400

    Bondage Poker - Nadia
    Bondage Poker - Nadia

--- BondageClub/Screens/Room/Poker/Poker.js
+++ BondageClub/Screens/Room/Poker/Poker.js
@@ -29,7 +29,7 @@ var PokerAsset = [
 	{
 		Family: "Model",
 		Type: "Set",
-		Opponent: ["Andrea", "Akira", "Becky", "Dita", "Emily", "Hannah", "Isanne", "Jasmine", "Jelena", "Masuimi", "Missey"]
+		Opponent: ["Andrea", "Akira", "Becky", "Dita", "Emily", "Hannah", "Isanne", "Jasmine", "Jelena", "Masuimi", "Missey", "Nadia"]
 	}
 ];
 var PokerPlayerCount = 4;
