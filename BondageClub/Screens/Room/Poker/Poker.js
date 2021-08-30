3081ffac6ec8a4f7c8f81f6fd39790c72d8e9cde
Ben987 <30805159+Ben987@users.noreply.github.com>
2021-08-30 12:18:02 -0400

    Bondage Poker - Jasmine
    Bondage Poker - Jasmine

--- BondageClub/Screens/Room/Poker/Poker.js
+++ BondageClub/Screens/Room/Poker/Poker.js
@@ -29,7 +29,7 @@ var PokerAsset = [
 	{
 		Family: "Model",
 		Type: "Set",
-		Opponent: ["Andrea", "Akira", "Becky", "Dita", "Emily", "Hannah", "Isanne"]
+		Opponent: ["Andrea", "Akira", "Becky", "Dita", "Emily", "Hannah", "Isanne", "Jasmine"]
 	}
 ];
 var PokerPlayerCount = 4;
