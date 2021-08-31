f4ff18a82ddb02114abefaaff45870a9bb46045e
Ben987 <30805159+Ben987@users.noreply.github.com>
2021-08-30 20:37:21 -0400

    Bondage Poker - Petra
    Bondage Poker - Petra

--- BondageClub/Screens/Room/Poker/Poker.js
+++ BondageClub/Screens/Room/Poker/Poker.js
@@ -29,7 +29,7 @@ var PokerAsset = [
 	{
 		Family: "Model",
 		Type: "Set",
-		Opponent: ["Andrea", "Akira", "Becky", "Dita", "Emily", "Hannah", "Isanne", "Jasmine", "Jelena", "Masuimi", "Missey", "Nadia", "Natalia"]
+		Opponent: ["Andrea", "Akira", "Becky", "Dita", "Emily", "Hannah", "Isanne", "Jasmine", "Jelena", "Masuimi", "Missey", "Nadia", "Natalia", "Petra"]
 	}
 ];
 var PokerPlayerCount = 4;
