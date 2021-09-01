00e144adc88d24af32f92dd1792e2501fb482ff3
Ben987 <30805159+Ben987@users.noreply.github.com>
2021-08-31 20:49:11 -0400

    Bondage Poker - Tigerr
    Bondage Poker - Tigerr

--- BondageClub/Screens/Room/Poker/Poker.js
+++ BondageClub/Screens/Room/Poker/Poker.js
@@ -29,7 +29,7 @@ var PokerAsset = [
 	{
 		Family: "Model",
 		Type: "Set",
-		Opponent: ["Andrea", "Akira", "Becky", "Dita", "Emily", "Hannah", "Isanne", "Jasmine", "Jelena", "Masuimi", "Missey", "Nadia", "Natalia", "Petra", "Sasha", "Supergirl", "Tasha"]
+		Opponent: ["Andrea", "Akira", "Becky", "Dita", "Emily", "Hannah", "Isanne", "Jasmine", "Jelena", "Masuimi", "Missey", "Nadia", "Natalia", "Petra", "Sasha", "Supergirl", "Tasha", "Tigerr"]
 	}
 ];
 var PokerPlayerCount = 4;
