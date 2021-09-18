255bf6cbeaabf2a6043aa5dc233072744441397f
Ben987 <30805159+Ben987@users.noreply.github.com>
2021-09-18 15:32:38 -0400

    Credits/Patrons update
    Credits/Patrons update

--- BondageClub/Screens/Character/Login/Login.js
+++ BondageClub/Screens/Character/Login/Login.js
@@ -6,11 +6,12 @@ var LoginCreditsPosition = 0;
 var LoginThankYou = "";
 /* eslint-disable */
 var LoginThankYouList = [
-						"Aceffect", "Anna", "Aylea", "BlueWinter", "Brian", "Bryce", "Chennifer", "Christian", "Clash", "DarkStar",
-						"Dini", "Edwin", "Elcriminal", "Epona", "Escurse", "FanRunner", "Greendragon", "JoeyDubDee", "KamiKaze", "Kimuriel",
-						"Longwave", "Michal", "Michel", "Mike", "Mindtie", "Misa", "MrUniver", "Nick", "Nightcore", "Rashiash",
-						"Ray", "Rika", "Robin", "Rutherford", "Ryner", "Samuel", "SeraDenoir", "Shadow", "Tam", "TBoneShark",
-						"TKSonix", "TopHat", "Troubadix", "William", "Xepherio", "Yuna", "Znarf"];
+	"Aceffect", "Anna", "Aylea", "Ben", "BlueWinter", "Brian", "bryce", "Chennifer", "Christian", "Clash", 
+	"DarkStar", "Dini", "Edwin", "Elcriminal", "Epona", "Escurse", "FanRunner", "Greendragon", "JoeyDubDee", "KamiKaze", 
+	"Kimuriel", "Longwave", "Michal", "Michel", "Mike", "Mindtie", "Misa", "MrUniver", "Nick", "Nightcore", 
+	"Ray", "Rika", "Robin", "Rutherford", "Ryner", "Sage", "Samuel", "SeraDenoir", "Shadow", "Tam", 
+	"TBoneShark", "TKSonix", "TopHat", "Troubadix", "Xepherio", "Yuna", "Znarf"
+];
 /* eslint-enable */
 var LoginThankYouNext = 0;
 var LoginSubmitted = false;
