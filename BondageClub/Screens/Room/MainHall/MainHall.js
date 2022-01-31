69fb41007c7862b529478d407c63c478505fe8fe
Ben987 <30805159+Ben987@users.noreply.github.com>
2022-01-31 17:14:40 -0500

    GGTS - No return to chat room at level 6
    GGTS - No return to chat room at level 6

--- BondageClub/Screens/Room/MainHall/MainHall.js
+++ BondageClub/Screens/Room/MainHall/MainHall.js
@@ -126,9 +126,8 @@ function MainHallLoad() {
 	MainHallBackground = Player.VisualSettings && Player.VisualSettings.MainHallBackground ? Player.VisualSettings.MainHallBackground : "MainHall";
 	MainHallStartEventTimer = null;
 	MainHallNextEventTimer = null;
-	if (!Player.ImmersionSettings.ReturnToChatRoom || Player.LastChatRoom === "" || MainHallBeingPunished ) {
+	if (!Player.ImmersionSettings.ReturnToChatRoom || (Player.LastChatRoom === "") || MainHallBeingPunished || (AsylumGGTSGetLevel(Player) >= 6))
 		MainHallMaid = CharacterLoadNPC("NPC_MainHall_Maid");
-	}
 	MainHallIsMaid = LogQuery("JoinedSorority", "Maid");
 	MainHallIsHeadMaid = LogQuery("LeadSorority", "Maid");
 	MainHallHasOwnerLock = InventoryCharacterHasOwnerOnlyRestraint(Player);
@@ -161,7 +160,7 @@ function MainHallRun() {
 	if (!MainHallBeingPunished) {
 
 		// We return to the last online chat room if possible
-		if (Player.ImmersionSettings && Player.LastChatRoom && Player.LastChatRoom != "" && (MainHallMaid === null || MainHallMaid.Stage === "0")) {
+		if (Player.ImmersionSettings && Player.LastChatRoom && (Player.LastChatRoom != "") && (AsylumGGTSGetLevel(Player) <= 5) && ((MainHallMaid === null) || (MainHallMaid.Stage === "0"))) {
 			if (MainHallFirstFrame) {
 				if (Player.ImmersionSettings.ReturnToChatRoom) {
 					ChatRoomStart("", "", "MainHall", "Introduction", BackgroundsTagList);
