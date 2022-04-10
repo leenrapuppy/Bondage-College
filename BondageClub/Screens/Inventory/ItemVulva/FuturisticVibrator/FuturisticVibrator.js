bfff0e4277b2836b6fe23b98a1795edbda1cff1d
remiliacn <bilibililvnima@gmail.com>
2022-04-09 21:18:59 -0700

    Enhancement: Allow CJK langs futuristic commands. (#3316)

--- BondageClub/Screens/Inventory/ItemVulva/FuturisticVibrator/FuturisticVibrator.js
+++ BondageClub/Screens/Inventory/ItemVulva/FuturisticVibrator/FuturisticVibrator.js
@@ -110,8 +110,6 @@ function InventoryItemVulvaFuturisticVibratorExit() {
 		ElementRemove("FuturisticVibe" + ItemVulvaFuturisticVibratorTriggers[I]);
 }
 
-
-
 function InventoryItemVulvaFuturisticVibratorDetectMsg(msg, TriggerValues) {
 	var commandsReceived = [];
 
@@ -126,7 +124,22 @@ function InventoryItemVulvaFuturisticVibratorDetectMsg(msg, TriggerValues) {
 		regexString = regexString.replace(/\*/g, ".*")//regexString.replaceAll("\\*", ".*")
 		regexString = regexString.toUpperCase()
 
-		const triggerRegex = new RegExp(`\\b${regexString}\\b`);
+		const nonLatinCharRegex = new RegExp('^([^\\x20-\\x7F]|\\\\.\\*)+$');
+		let triggerRegex;
+
+		// In general, in most of the Asian language, the full sentence will be considered as one whole word
+		// Because how regex consider word boundaries to be position between \w -> [A-Za-z0-9_] and \W. 
+
+		// So if commands are set to those languages, the command will never be triggered.
+		// Or if the command is not a word 
+		// This enhancement should allow Asian language commands, and also emoji/special characters
+		// (e.g. A symbol such as ↑ or ↓, Languages in CJK group such as Chinese, Japanese, and Korean.)
+		// This should be a fun addition to boost the user's experience.
+		if (nonLatinCharRegex.test(regexString)) {
+			triggerRegex = new RegExp(regexString);
+		} else {
+			triggerRegex = new RegExp(`\\b${regexString}\\b`);
+		}
 		const success = triggerRegex.test(msg);
 
 		if (success) commandsReceived.push(ItemVulvaFuturisticVibratorTriggers[I]);
