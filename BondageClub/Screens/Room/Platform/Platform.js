99c93ec6e982fc66b23f3a69ba4bb6bea217feec
BondageProjects <rainfroi@videotron.ca>
2022-07-05 15:24:57 -0400

    Bondage Brawl - Olivia Stand Attack

--- BondageClub/Screens/Room/Platform/Platform.js
+++ BondageClub/Screens/Room/Platform/Platform.js
@@ -98,7 +98,11 @@ var PlatformTemplate = [
 			{ Name: "Crouch", Cycle: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1], Speed: 110 },
 			{ Name: "Crawl", Cycle: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], Speed: 30 },
 			{ Name: "Bound", Cycle: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1], Speed: 110 },
-			{ Name: "Stun", Cycle: [0], Speed: 1000 }
+			{ Name: "Stun", Cycle: [0], Speed: 1000 },
+			{ Name: "StandAttackFast", Cycle: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], Speed: 17 }
+		],
+		Attack: [
+			{ Name: "StandAttackFast", HitBox: [0.7, 0.15, 0.9, 0.3], HitAnimation: [6, 7, 8, 9, 10], Damage: [1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8], Speed: 300 }
 		]
 	},
 	{
