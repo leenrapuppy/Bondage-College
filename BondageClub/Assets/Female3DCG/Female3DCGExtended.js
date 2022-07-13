f789f21d4170b4d5dabaa4bbcb4f1ac38aa132c4
MoonlightGleam <dandaruart@gmail.com>
2022-07-14 01:47:02 +0200

    Added ability to change stickers on the post

--- BondageClub/Assets/Female3DCG/Female3DCGExtended.js
+++ BondageClub/Assets/Female3DCG/Female3DCGExtended.js
@@ -2163,6 +2163,19 @@ var AssetFemale3DCGExtended = {
 						]
 					},
 					{
+						Name: "Sticker", Key: "s",
+						Options: [
+							{}, //s0 - Paw
+							{}, //s1 - Triskel
+							{}, //s2 - Moon
+							{}, //s3 - LGBT
+							{}, //s4 - Trans
+							{}, //s5 - Bi
+							{}, //s6 - NoSwim
+							{}, //s7 - None
+						]
+					},
+					{
 						Name: "Txt", Key: "x",
 						Options: [{ HasSubscreen: true }],
 					},
