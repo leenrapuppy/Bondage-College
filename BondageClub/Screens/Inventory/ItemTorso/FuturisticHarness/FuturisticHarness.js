8b2b76a23231d8cb04df3c777a7e28ba1f128ae5
banarama92 <35132-bananarama92@users.noreply.gitgud.io>
2022-10-26 05:50:31 +0200

    MAINT: Convert the futuristic harness into a typed item

--- BondageClub/Screens/Inventory/ItemTorso/FuturisticHarness/FuturisticHarness.js
+++ BondageClub/Screens/Inventory/ItemTorso/FuturisticHarness/FuturisticHarness.js
@@ -1,71 +1,36 @@
 "use strict";
 
-var InventoryItemTorsoFuturisticHarnessOptions = [
-	{
-		Name: "Full",
-		Property: { Type: null, Difficulty: 2},
-	},
-	{
-		Name: "Upper",
-		Property: { Type: "Upper", Difficulty: 0},
-	},
-	{
-		Name: "Lower",
-		Property: { Type: "Lower", Difficulty: 0},
-	},
-];
-
-// Loads the item extension properties
-function InventoryItemTorsoFuturisticHarnessLoad() {
-	var C = CharacterGetCurrent();
-	if (InventoryItemFuturisticValidate(C) !== "") {
-		InventoryItemFuturisticLoadAccessDenied();
-	}  else
-		ExtendedItemLoad(InventoryItemTorsoFuturisticHarnessOptions, "FuturisticHarnessType");
+/**
+ * Draw the item extension screen
+ * @param {() => void} OriginalFunction - The function that is normally called when an archetypical item reaches this point.
+ * @returns {void} - Nothing
+ */
+function InventoryItemTorsoFuturisticHarnessDraw(OriginalFunction) {
+	if (!FuturisticAccessDraw(OriginalFunction)) {
+		return;
+	}
+	const C = CharacterGetCurrent();
+	const FuturisticCollarItems = InventoryItemNeckFuturisticCollarGetItems(C);
+	if (FuturisticCollarItems.length > 0) {
+		DrawButton(1385, 800, 225, 55, DialogFindPlayer("FuturisticCollarColor"), "White");
+	}
 }
 
-// Draw the item extension screen
-function InventoryItemTorsoFuturisticHarnessDraw() {
-	var C = CharacterGetCurrent();
-	if (InventoryItemFuturisticValidate(C) !== "") {
-		InventoryItemFuturisticDrawAccessDenied();
-	} else {
-		ExtendedItemDraw(InventoryItemTorsoFuturisticHarnessOptions, "FuturisticHarnessType");
-
-		DrawAssetPreview(1387, 75, DialogFocusItem.Asset);
-
-		var FuturisticCollarItems = InventoryItemNeckFuturisticCollarGetItems(C);
-
-		if (FuturisticCollarItems.length > 0) {
-			DrawButton(1400, 910, 200, 55, DialogFindPlayer("FuturisticCollarColor"), "White");
+/**
+ * Catches the item extension clicks
+ * @param {() => void} OriginalFunction - The function that is normally called when an archetypical item reaches this point.
+ * @returns {void} - Nothing
+ */
+function InventoryItemTorsoFuturisticHarnessClick(OriginalFunction) {
+	if (!FuturisticAccessClick(OriginalFunction)) {
+		return;
+	}
+	if (MouseIn(1385, 800, 225, 55)) {
+		const C = CharacterGetCurrent();
+		const FuturisticCollarItems = InventoryItemNeckFuturisticCollarGetItems(C);
+		if (FuturisticCollarItems.length > 0 && DialogFocusItem) {
+			InventoryItemNeckFuturisticCollarColor(C, DialogFocusItem);
+			FuturisticAccessExit();
 		}
 	}
-}
-
-
-function InventoryItemTorsoFuturisticHarnessPublishAction(C, Option) {
-	var msg = "FuturisticHarnessSet" + Option.Name;
-	var Dictionary = [
-		{ Tag: "SourceCharacter", Text: CharacterNickname(Player), MemberNumber: Player.MemberNumber },
-		{ Tag: "DestinationCharacter", Text: CharacterNickname(C), MemberNumber: C.MemberNumber },
-	];
-	ChatRoomPublishCustomAction(msg, true, Dictionary);
-}
-
-// Catches the item extension clicks
-function InventoryItemTorsoFuturisticHarnessClick() {
-	var C = CharacterGetCurrent();
-	if (InventoryItemFuturisticValidate(C) !== "") {
-		InventoryItemFuturisticClickAccessDenied();
-	} else {
-
-		ExtendedItemClick(InventoryItemTorsoFuturisticHarnessOptions);
-
-		var FuturisticCollarItems = InventoryItemNeckFuturisticCollarGetItems(C);
-		if (MouseIn(1400, 910, 200, 55) && FuturisticCollarItems.length > 0 && DialogFocusItem) { InventoryItemNeckFuturisticCollarColor(C, DialogFocusItem); InventoryItemTorsoFuturisticHarnessExit();}
-	}
-}
-
-function InventoryItemTorsoFuturisticHarnessExit() {
-	InventoryItemFuturisticExitAccessDenied();
 }
