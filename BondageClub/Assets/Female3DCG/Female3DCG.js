967998c1fb47fe7aecd8c2b2d1174481a10c254d
Ayesha-678 <59668834+Ayesha-678@users.noreply.github.com>
2022-11-08 20:47:09 -0600

    Male Tank Top1

--- BondageClub/Assets/Female3DCG/Female3DCG.js
+++ BondageClub/Assets/Female3DCG/Female3DCG.js
@@ -283,6 +283,9 @@ var AssetFemale3DCG = [
 				],
 			},
 			// Flat Chest Compatible Clothing
+			{
+				Name: "MaleTankTop1", Gender: "M", Prerequisite: ["HasFlatChest"], HideItem: ["ItemNipplesPiercingsRoundPiercing", "ItemNipplesPiercingsNecklacePiercingChain", "ItemNipplesLactationPump", "BraRibbons", "ItemBreastRibbons"]
+			}
 		],
 		Color: ["Default", "#202020", "#808080", "#bbbbbb", "#aa8080", "#80aa80", "#8080aa", "#aaaa80", "#80aaaa", "#aa80aa", "#cc3333", "#33cc33", "#3333cc", "#cccc33", "#33cccc", "#cc33cc"]
 	},
