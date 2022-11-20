90925c33549a58374d6cdda1cc816ea9329f228a
bananarama92 <35132-bananarama92@users.noreply.gitgud.io>
2022-11-20 21:24:20 +0100

    MAINT: Make the crafting validationg of `Type` and `Color` less strict
    We can't reliably validate w.r.t. `Asset.AllowTypes` here, as there are potentially multiple assets with the same name but distinct allowed types (e.g. the SturdyLeatherBelt and Ribbons)

--- BondageClub/Screens/Room/Crafting/Crafting.js
+++ BondageClub/Screens/Room/Crafting/Crafting.js
@@ -811,10 +811,7 @@ function CraftingItemListBuild() {
 				return true
 			} else {
 				const Colors = c.Color.replace(" ", "").split(",");
-				return (
-					Colors.every((c) => CommonIsColor(c) || (c === "Default"))
-					&& (Colors.length <= a.ColorableLayerCount)
-				);
+				return Colors.every((c) => CommonIsColor(c) || (c === "Default"));
 			}
 		},
 		GetDefault: function(c, a) {
@@ -823,7 +820,7 @@ function CraftingItemListBuild() {
 			} else {
 				const Colors = c.Color.replace(" ", "").split(",");
 				const ColorsNew = Colors.map((c) => CommonIsColor(c) ? c : "Default");
-				return ColorsNew.slice(0, a.ColorableLayerCount).join(",");
+				return ColorsNew.join(",");
 			}
 		},
 		StatusCode: CraftingStatusCode.ERROR,
@@ -890,15 +887,9 @@ function CraftingItemListBuild() {
 	},
 	Type: {
 		Validate: function (c, a) {
-			if (a == null) {
-				return true;
-			} else if ((a.Archetype === ExtendedArchetype.TYPED) && (c.Type == null)) {
-				return true;
-			} else if (a.AllowType != null) {
-				return (a.AllowType && a.AllowType.includes(c.Type));
-			} else {
-				return c.Type == null;
-			}
+			// We can't reliably validate w.r.t. `Asset.AllowTypes` here, as there are potentially multiple assets with
+			// the same name but distinct types (e.g. the SturdyLeatherBelt and Ribbons)
+			return (c.Type == null) || (typeof c.Type === "string");
 		},
 		GetDefault: function (c, a) {
 			if (a == null) {
