leenrapuppy/Bondage-College

commit c0eb5e9b32f5c67301efdbaa357dd5b99ff76573

Fix ESlint warnings

BondageClub/Scripts/TypedItem.js
@@ -540,8 +540,7 @@ function TypedItemSetRandomOption(C, itemOrGroupName, push = false) {
 	const allOptions = TypedItemGetOptions(item.Asset.Group.Name, item.Asset.Name);
 	// Avoid blocked & non-random options
 	const availableOptions = allOptions
-		.filter(option => option.Random !== false)
-		.filter(option => !InventoryBlockedOrLimited(C, item, option.Property.Type));
+		.filter(o => o.Random !== false || !InventoryBlockedOrLimited(C, item, o.Property.Type));
 
 	/** @type {ExtendedItemOption} */
 	let option;
@@ -568,7 +567,7 @@ function TypedItemCustomChatPrefix(Name, Data) {
 			newOption: { OptionType: "ExtendedItemOption", Name: Name, Property: { Type: Name } },
 			previousIndex: 0,
 			newIndex: 0,
-		})
+		});
 	} else {
 		return Data.dialog.chatPrefix;
 	}

BondageClub/Assets/Female3DCG/Female3DCG.js
@@ -7374,7 +7374,7 @@ var AssetFemale3DCG = [
 					{ Name: "FillHoleShapedMouth", HasType: false, AllowModuleTypes: ["m3f3","m7f3"], CopyLayerColor: "FillRoundMouth" },
 					{ Name: "FillHoleShapedMouthTransparent", HasType: false, AllowModuleTypes: ["m3f4","m7f4"], CopyLayerColor: "FillRoundMouth" },
 					{ Name: "FillShapedMouthHighlight", HasType: false, AllowModuleTypes: ["m3f1","m7f1","m3f2","m7f2","m3f3","m7f3","m3f4","m7f4"], CopyLayerColor: "PanelHeadHighlight" },
-					
+
 					// Linings
 					{ Name: "LiningRoundFace", HasType: false, ColorGroup: "Linings" }, // master colour for face lining
 					{ Name: "LiningRoundFaceHighlight", HasType: false, CopyLayerColor: "PanelHeadHighlight" },

BondageClub/Assets/Female3DCG/Female3DCGExtended.js
@@ -197,7 +197,7 @@ var AssetFemale3DCGExtended = {
 			},
 		}, //BallCapFront
 	},
-		LeftHand: {
+	LeftHand: {
 		Rings: {
 			Archetype: ExtendedArchetype.MODULAR,
 			Config: {
@@ -401,18 +401,18 @@ var AssetFemale3DCGExtended = {
 					{
 						Name: "Cloth", Key: "c",
 						Options: [
-						{}, //c0 - Base
-						{}, //c1 - BaseStained
-						{}, //c2 - Torn1
-						{}, //c3 - Torn1Stained
+							{}, //c0 - Base
+							{}, //c1 - BaseStained
+							{}, //c2 - Torn1
+							{}, //c3 - Torn1Stained
 						],
 					},
 					{
 						Name: "Belt", Key: "b",
 						Options: [
-						{}, //b0
-						{}, //b1
-						{}, //b2
+							{}, //b0
+							{}, //b1
+							{}, //b2
 						],
 					},
 				],
@@ -2757,15 +2757,15 @@ var AssetFemale3DCGExtended = {
 					{
 						Name: "Plaque", Key: "p",
 						Options: [
-						{}, //p0 - Border
-						{}, //p1 - Border
+							{}, //p0 - Border
+							{}, //p1 - Border
 						]
 					},
 					{
 						Name: "Dirt", Key: "d",
 						Options: [
-						{}, //d0 - Clean
-						{}, //d1 - Dirty
+							{}, //d0 - Clean
+							{}, //d1 - Dirty
 						]
 					},
 					{
@@ -2792,8 +2792,8 @@ var AssetFemale3DCGExtended = {
 					{
 						Name: "PostIt", Key: "m",
 						Options: [
-						{}, //m0 - Postit
-						{}, //m1 - No PostIt
+							{}, //m0 - Postit
+							{}, //m1 - No PostIt
 						]
 					},
 					{
@@ -3352,15 +3352,17 @@ var AssetFemale3DCGExtended = {
 						Name: "MPanel", Key: "m", DrawImages: false,
 						Options:[
 							{ // m0 - No Mouth
-								Property: { Effect: ["GagEasy","BlockMouth"],
-											OverridePriority: 38,
-											Block: ["ItemMouth", "ItemMouth2", "ItemMouth3"]
+								Property: {
+									Effect: ["GagEasy","BlockMouth"],
+									OverridePriority: 38,
+									Block: ["ItemMouth", "ItemMouth2", "ItemMouth3"]
 								}
 							},
 							{ // m1 - Hole Only
-								Property: { Effect: ["BlockMouth"],
-											OverridePriority: 38,
-											Block: ["ItemMouth", "ItemMouth2", "ItemMouth3"]
+								Property: {
+									Effect: ["BlockMouth"],
+									OverridePriority: 38,
+									Block: ["ItemMouth", "ItemMouth2", "ItemMouth3"]
 								}
 							},
 							{ // m2 - Round Mouth
@@ -3370,15 +3372,17 @@ var AssetFemale3DCGExtended = {
 								Property: { Effect: ["OpenMouth"] }
 							},
 							{ // m4 - No Mouth Transparent
-								Property: { Effect: ["GagEasy","BlockMouth"],
-											OverridePriority: 38,
-											Block: ["ItemMouth", "ItemMouth2", "ItemMouth3"]
+								Property: {
+									Effect: ["GagEasy","BlockMouth"],
+									OverridePriority: 38,
+									Block: ["ItemMouth", "ItemMouth2", "ItemMouth3"]
 								}
 							},
 							{ // m5 - Hole Only Transparent
-								Property: { Effect: ["BlockMouth"],
-											OverridePriority: 38,
-											Block: ["ItemMouth", "ItemMouth2", "ItemMouth3"]
+								Property: {
+									Effect: ["BlockMouth"],
+									OverridePriority: 38,
+									Block: ["ItemMouth", "ItemMouth2", "ItemMouth3"]
 								}
 							},
 							{ // m6 - Round Mouth Transparent
@@ -3431,12 +3435,12 @@ var AssetFemale3DCGExtended = {
 						Options:[{},{},] // Opaque, Transparent
 					},
 					//{ //separate keys to control transparency, may be useful later
-					//	Name: "Mouth Transparency", Key: "y", DrawImages: false,
-					//	Options:[{},{},] // Opaque, Transparent
+					// Name: "Mouth Transparency", Key: "y", DrawImages: false,
+					// Options:[{},{},] // Opaque, Transparent
 					//},
 					//{
-					//	Name: "Eye Transparency", Key: "w", DrawImages: false,
-					//	Options:[{},{},] // Opaque, Transparent
+					// Name: "Eye Transparency", Key: "w", DrawImages: false,
+					// Options:[{},{},] // Opaque, Transparent
 					//},
 					{
 						Name: "MFill", Key: "f", DrawImages: false,
@@ -3445,23 +3449,27 @@ var AssetFemale3DCGExtended = {
 								Property: { Effect: [""] }
 							},
 							{// f1 - Opaque
-								Property: { Effect: ["BlockMouth"],
-											OverridePriority: 38
+								Property: {
+									Effect: ["BlockMouth"],
+									OverridePriority: 38
 								}
 							},
 							{// f2 - Transparent
-								Property: { Effect: ["BlockMouth"],
-											OverridePriority: 38
+								Property: {
+									Effect: ["BlockMouth"],
+									OverridePriority: 38
 								}
 							},
 							{// f3 - Opaque Hole
-								Property: { Effect: ["BlockMouth"],
-											OverridePriority: 38
+								Property: {
+									Effect: ["BlockMouth"],
+									OverridePriority: 38
 								}
 							},
 							{// f4 - Transparent Hole
-								Property: { Effect: ["BlockMouth"],
-											OverridePriority: 38
+								Property: {
+									Effect: ["BlockMouth"],
+									OverridePriority: 38
 								}
 							},
 						]
@@ -3521,33 +3529,33 @@ var AssetFemale3DCGExtended = {
 							{// z1 - Open
 								Prerequisite: ["EyesEmpty"],
 								Property: { Effect: [""],
-								Hide: ["HairFront"],
-								HideItem: ["MaskOpenFaceHood", "MaskFaceVeil", "MaskFoxMask"],
-								Block: ["ItemHead"],
+									Hide: ["HairFront"],
+									HideItem: ["MaskOpenFaceHood", "MaskFaceVeil", "MaskFoxMask"],
+									Block: ["ItemHead"],
 								}
 							},
 							{// z2 - Closed
 								Prerequisite: ["EyesEmpty"],
 								Property: {
-								Effect: ["BlindHeavy","GagEasy","BlockMouth"],
-								Hide: ["HairFront","Mask"],
-								Block: ["ItemMouth", "ItemMouth2", "ItemMouth3", "ItemHead", "ItemNose"],
+									Effect: ["BlindHeavy","GagEasy","BlockMouth"],
+									Hide: ["HairFront","Mask"],
+									Block: ["ItemMouth", "ItemMouth2", "ItemMouth3", "ItemHead", "ItemNose"],
 								}
 							},
 							{// z3 - Open Transparent
 								Prerequisite: ["EyesEmpty"],
 								Property: { Effect: [""],
-								Hide: ["HairFront"],
-								HideItem: ["MaskOpenFaceHood", "MaskFaceVeil", "MaskFoxMask"],
-								Block: ["ItemHead"],
+									Hide: ["HairFront"],
+									HideItem: ["MaskOpenFaceHood", "MaskFaceVeil", "MaskFoxMask"],
+									Block: ["ItemHead"],
 								}
 							},
 							{// z4 - Closed Transparent
 								Prerequisite: ["EyesEmpty"],
 								Property: { Effect: ["BlindLight","GagEasy","BlockMouth"],
-								Hide: ["HairFront"],
-								HideItem: ["MaskOpenFaceHood", "MaskFaceVeil", "MaskFoxMask"],
-								Block: ["ItemMouth", "ItemMouth2", "ItemMouth3", "ItemHead", "ItemNose"],
+									Hide: ["HairFront"],
+									HideItem: ["MaskOpenFaceHood", "MaskFaceVeil", "MaskFoxMask"],
+									Block: ["ItemMouth", "ItemMouth2", "ItemMouth3", "ItemHead", "ItemNose"],
 								}
 							},
 						]
@@ -6544,38 +6552,37 @@ var AssetFemale3DCGExtended = {
 			},
 		}, //StitchGag
 		LatexRespirator: {
-            Archetype: ExtendedArchetype.MODULAR,
-            Config: {
-                ChatSetting: ModularItemChatSetting.PER_MODULE,
-                Modules: [
-                    {
-                        Name: "Filter", Key: "f",
-                        Options: [{}, {}, {}, {}, ] // None,Filter,SmallTubes,LargeTubes
-                    },
-                    {
-                        Name: "Glow", Key: "g",
-                        Options: [ {}, {},] //  Glow off or on
-                    },
-                    {
-                        Name: "Straps", Key: "s",
-                        Options: [ {}, {},] // No Straps, Straps
-                    },
-                    {
-                        Name: "Mask", Key: "m",
-                        Options: [
+			Archetype: ExtendedArchetype.MODULAR,
+			Config: {
+				ChatSetting: ModularItemChatSetting.PER_MODULE,
+				Modules: [
+					{
+						Name: "Filter", Key: "f",
+						Options: [{}, {}, {}, {}, ] // None,Filter,SmallTubes,LargeTubes
+					},
+					{
+						Name: "Glow", Key: "g",
+						Options: [ {}, {},] //  Glow off or on
+					},
+					{
+						Name: "Straps", Key: "s",
+						Options: [ {}, {},] // No Straps, Straps
+					},
+					{
+						Name: "Mask", Key: "m",
+						Options: [
 							{Property: {Effect: ["BlockMouth"]}},
 							{Property: {Effect: ["BlockMouth", "GagVeryLight"]}},
 							{Property: {Effect:[""]}},
-
 						] // Mask, Thick Filters, No Mask
-                    },
+					},
 					{
 						Name: "Length", Key: "l",
 						Options: [{},{}] // Behind Hair, Over Hair
 					},
-                ]
-            }
-        }, //LatexRespirator
+				]
+			}
+		}, //LatexRespirator
 	}, // ItemMouth3
 	Mask: {
 		BunnyMask1: {
@@ -6789,7 +6796,7 @@ var AssetFemale3DCGExtended = {
 						Prerequisite: ["NotSuspended", "CanKneel"],
 						Random: false,
 					},
-			],
+				],
 				Dialog: {
 					Load: "SelectRopeBondage",
 					TypePrefix: "RopeBondage",
@@ -7450,15 +7457,15 @@ var AssetFemale3DCGExtended = {
 					{
 						Name: "Plaque", Key: "p",
 						Options: [
-						{}, //p0 - Border
-						{}, //p1 - Border
+							{}, //p0 - Border
+							{}, //p1 - Border
 						]
 					},
 					{
 						Name: "Dirt", Key: "d",
 						Options: [
-						{}, //d0 - Clean
-						{}, //d1 - Dirty
+							{}, //d0 - Clean
+							{}, //d1 - Dirty
 						]
 					},
 					{
@@ -7477,8 +7484,8 @@ var AssetFemale3DCGExtended = {
 					{
 						Name: "PostIt", Key: "m",
 						Options: [
-						{}, //m0 - Postit
-						{}, //m1 - No PostIt
+							{}, //m0 - Postit
+							{}, //m1 - No PostIt
 						]
 					},
 					{

BondageClub/Scripts/ExtendedItem.js
@@ -39,14 +39,14 @@ const ExtendedXY = [
  */
 const ExtendedXYWithoutImages = [
 	[], //0 placeholder
-    [[1385, 450]], //1 option per page
-    [[1260, 450], [1510, 450]], //2 options per page
-    [[1135, 450], [1385, 450], [1635, 450]], //3 options per page
-    [[1260, 450], [1510, 450], [1260, 525], [1510, 525]], //4 options per page
-    [[1135, 450], [1385, 450], [1635, 450], [1260, 525], [1510, 525]], //5 options per page
-    [[1135, 450], [1385, 450], [1635, 450], [1135, 525], [1385, 525], [1635, 525]], //6 options per page
-    [[1010, 450], [1260, 450], [1510, 450], [1760, 450], [1135, 525], [1385, 525], [1635, 525]], //7 options per page
-    [[1010, 450], [1260, 450], [1510, 450], [1760, 450], [1010, 525], [1260, 525], [1510, 525], [1760, 525]], //8 options per page
+	[[1385, 450]], //1 option per page
+	[[1260, 450], [1510, 450]], //2 options per page
+	[[1135, 450], [1385, 450], [1635, 450]], //3 options per page
+	[[1260, 450], [1510, 450], [1260, 525], [1510, 525]], //4 options per page
+	[[1135, 450], [1385, 450], [1635, 450], [1260, 525], [1510, 525]], //5 options per page
+	[[1135, 450], [1385, 450], [1635, 450], [1135, 525], [1385, 525], [1635, 525]], //6 options per page
+	[[1010, 450], [1260, 450], [1510, 450], [1760, 450], [1135, 525], [1385, 525], [1635, 525]], //7 options per page
+	[[1010, 450], [1260, 450], [1510, 450], [1760, 450], [1010, 525], [1260, 525], [1510, 525], [1760, 525]], //8 options per page
 ];
 
 /**
@@ -121,7 +121,7 @@ function ExtendedItemLoad(Options, DialogKey, BaselineProperty=null) {
 			DialogFocusItem.Property = Object.assign(
 				DialogFocusItem.Property,
 				(InitialProperty != null) ? JSON.parse(JSON.stringify(InitialProperty)) : {},
-			)
+			);
 			const RefreshDialog = (CurrentScreen != "Crafting");
 			CharacterRefresh(C, true, RefreshDialog);
 			ChatRoomCharacterItemUpdate(C, DialogFocusItem.Asset.Group.Name);
@@ -239,7 +239,7 @@ function ExtendedItemDrawButton(Option, CurrentOption, DialogPrefix, X, Y, ShowI
 	const ButtonColor = ExtendedItemGetButtonColor(C, Option, CurrentOption, Hover, IsSelected, Item);
 	DrawButton(X, Y, 225, 55 + ImageHeight, "", ButtonColor, null, null, IsSelected);
 	if (ShowImages) {
-		DrawImageResize(AssetSource, X + 2, Y, 221, 221)
+		DrawImageResize(AssetSource, X + 2, Y, 221, 221);
 		if (Option.OptionType !== "ModularItemModule") {
 			DrawPreviewIcons(ExtendItemGetIcons(C, Asset, Type), X + 2, Y);
 		}
@@ -725,9 +725,9 @@ function ExtendedItemCreateValidateFunction(functionPrefix, ValidationCallback, 
 		if (!changeWhenLocked && itemLocked && !DialogCanUnlock(C, item)) {
 			return DialogFindPlayer("CantChangeWhileLocked");
 		} else {
-			return ExtendedItemValidate(C, item, option, currentOption)
+			return ExtendedItemValidate(C, item, option, currentOption);
 		}
-	}
+	};
 
 	if (ValidationCallback) {
 		window[validateFunctionName] = function (C, item, option, currentOption) {

BondageClub/Scripts/ModularItem.js
@@ -122,8 +122,7 @@ function ModularItemCreateDrawFunction(data) {
 	const drawFunctionName = `${data.functionPrefix}Draw`;
 	const drawFunction = function () {
 		const currentModule = data.currentModule || ModularItemBase;
-		const drawFunction = data.drawFunctions[currentModule];
-		return drawFunction();
+		return data.drawFunctions[currentModule]();
 	};
 	if (data.scriptHooks && data.scriptHooks.draw) {
 		window[drawFunctionName] = function () {
@@ -141,8 +140,7 @@ function ModularItemCreateClickFunction(data) {
 	const clickFunctionName = `${data.functionPrefix}Click`;
 	const clickFunction = function () {
 		const currentModule = data.currentModule || ModularItemBase;
-		const clickFunction = data.clickFunctions[currentModule];
-		return clickFunction();
+		return data.clickFunctions[currentModule]();
 	};
 	if (data.scriptHooks && data.scriptHooks.click) {
 		window[clickFunctionName] = function () {
@@ -177,7 +175,7 @@ function ModularItemUpdateModules(Modules) {
 		mod.Options.forEach((option, i) => {
 			option.Name = `${mod.Key}${i}`;
 			option.OptionType = "ModularItemOption";
-		})
+		});
 	}
 	return /** @type {ModularItemModule[]} */(Modules);
 }
@@ -266,7 +264,7 @@ function ModularItemCreateDrawData(itemCount, asset, drawImages) {
 	const pageCount = Math.ceil(itemCount / itemsPerPage);
 
 	/** @type {[number, number][]} */
-	const positions = []
+	const positions = [];
 	let i = 0;
 	while (i < itemCount) {
 		i += itemsPerPage;
@@ -536,7 +534,7 @@ function ModularItemMergeModuleValues({ asset, modules }, moduleValues, Baseline
 		AllowActivity: Array.isArray(asset.AllowActivity) ? asset.AllowActivity.slice() : [],
 		Attribute: Array.isArray(asset.Attribute) ? asset.Attribute.slice() : [],
 		Tint: asset.AllowTint ? Array.isArray(asset.Tint) ? asset.Tint.slice() : [] : undefined,
-	})
+	});
 	if (BaselineProperty != null) {
 		ModularItemSanitizeProperties(BaselineProperty, BaseLineProperties, asset);
 	}
@@ -910,13 +908,13 @@ function ModularItemHideElement(ID, Module) {
 		return ModularItemModuleIsActive(Module);
 	}
 
-    if (ModularItemModuleIsActive(Module)) {
-        Element.style.display = "block";
-        return true;
-    } else {
-        Element.style.display = "none";
-        return false;
-    }
+	if (ModularItemModuleIsActive(Module)) {
+		Element.style.display = "block";
+		return true;
+	} else {
+		Element.style.display = "none";
+		return false;
+	}
 }
 
 /**
@@ -933,7 +931,7 @@ function ModularItemCustomChatPrefix(Name, Data) {
 			newOption: { OptionType: "ModularItemOption", Name: Name },
 			previousIndex: 0,
 			newIndex: 0,
-		})
+		});
 	} else {
 		return Data.chatMessagePrefix;
 	}

commit 2fdb35e92da0052c3d2046f3e6a03339f34302bb

Change OR into an AND
This fixes a bug I introduced in c0eb5e9b32f5c67301efdbaa357dd5b99ff76573
when merging the two filters together. We definitely only want randomly-
selectable, non-blocked options when setting one at random.

BondageClub/Scripts/TypedItem.js
@@ -540,7 +540,7 @@ function TypedItemSetRandomOption(C, itemOrGroupName, push = false) {
 	const allOptions = TypedItemGetOptions(item.Asset.Group.Name, item.Asset.Name);
 	// Avoid blocked & non-random options
 	const availableOptions = allOptions
-		.filter(o => o.Random !== false || !InventoryBlockedOrLimited(C, item, o.Property.Type));
+		.filter(o => o.Random !== false && !InventoryBlockedOrLimited(C, item, o.Property.Type));
 
 	/** @type {ExtendedItemOption} */
 	let option;