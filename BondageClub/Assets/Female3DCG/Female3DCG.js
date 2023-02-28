f199f8eb77626d2b71d884bd8d1236144d60ce42
BondageProjects <rainfroi@videotron.ca>
2023-02-28 17:50:46 -0500

    Wheel of Fortune - Hide in Wardrobe

--- BondageClub/Assets/Female3DCG/Female3DCG.js
+++ BondageClub/Assets/Female3DCG/Female3DCG.js
@@ -8509,6 +8509,7 @@ var AssetFemale3DCG = [
 			},
 			{
 				Name: "WheelFortune", Priority: 62, Top: 0, Left: 350, Value: 100, Difficulty: -10, Time: 5, IsRestraint: false, Effect: [],
+				NotVisibleOnScreen: ["Appearance", "Wardrobe"],
 				FixedPosition: true, Extended: true,
 				DefaultColor: ["Default", "#7F4141", "#881818", "#FFA8DD", "#2C2C2C", "Default", "#7B3D9E", "#FF3F3F", "#343434", "#F2F2F2", "#D43434", "#C2B556", "#81DA88", "#E18DC3"],
 				HasType: false
