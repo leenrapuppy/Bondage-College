23bac5b4546f2912bc43964c1fedb1f8dfda77f9
shion <Antichrist969@gmail.com>
2023-04-10 14:23:51 -0700

    bra

--- BondageClub/Assets/Female3DCG/Female3DCG.js
+++ BondageClub/Assets/Female3DCG/Female3DCG.js
@@ -4609,14 +4609,14 @@ var AssetFemale3DCG = [
 					{ Name: "Gems" }
 				]
 			},
-			{ Name: "Ribbons", Gender: "F", Fetish: ["Lingerie"], Value: 30, Difficulty: 3, Time: 10, RemoveTime: 5, BuyGroup: "Ribbon", Prerequisite: ["AccessBreast", "HasBreasts"], Extended: true },
+			{ Name: "Ribbons", Gender: "F", Fetish: ["Lingerie"], Value: 30, Difficulty: 3, Time: 10, RemoveTime: 5, BuyGroup: "Ribbon", Prerequisite: ["AccessBreast", "HasBreasts"], },
 			{ Name: "LeatherBreastBinder", Gender: "F", Fetish: ["Leather"], Value: 30, Difficulty: 5, Time: 15, BuyGroup: "BreastBinder", RemoveTime: 10, AllowLock: true, AllowTighten: true, Prerequisite: ["AccessTorso", "HasBreasts"] },
-			{ Name: "TickleBra", Gender: "F", Fetish: ["Latex"], Value: 100, Difficulty: 50, Time: 15, AllowLock: true, Prerequisite: ["AccessBreast", "HasBreasts"], Hide: ["ItemNipples", "ItemNipplesPiercings"], Effect: ["BreastChaste"], Block: ["ItemNipples", "ItemNipplesPiercings"], ExpressionTrigger: [{ Name: "Soft", Group: "Eyebrows", Timer: 10 }], Extended: true,
+			{ Name: "TickleBra", Gender: "F", Fetish: ["Latex"], Value: 100, Difficulty: 50, Time: 15, AllowLock: true, Prerequisite: ["AccessBreast", "HasBreasts"], Hide: ["ItemNipples", "ItemNipplesPiercings"], Effect: ["BreastChaste"], Block: ["ItemNipples", "ItemNipplesPiercings"], ExpressionTrigger: [{ Name: "Soft", Group: "Eyebrows", Timer: 10 }], 
 				Layer: [
-					{ Name: "TickleBraCup", AllowColorize: true , HasType: false,  Left: 0, Top: 0},
-					{ Name: "TickleUnit" , AllowColorize: true, HasType: false,  Left: 0, Top: 0},
-					{ Name: "Lining" , AllowColorize: true , HasType: false,  Left: 0, Top: 0},
-					{ Name: "wire" , AllowColorize: true, HasType: false,  Left: 0, Top: 0},
+					{ Name: "TickleBraCup", AllowColorize: true , HasType: false,},
+					{ Name: "TickleUnit" , AllowColorize: true, HasType: false,},
+					{ Name: "Lining" , AllowColorize: true , HasType: false,},
+					{ Name: "wire" , AllowColorize: true, HasType: false,},
 	
 				]
 			},
