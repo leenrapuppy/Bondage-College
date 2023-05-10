0776f23144b038aead48518fad28a322703e95e8
bananarama92 <bananarama921@outlook.com>
2023-05-10 20:22:29 +0200

    MAINT: Deduplicate Owner/Lovers/Family lock code

--- BondageClub/Screens/Inventory/ItemMisc/FamilyPadlock/FamilyPadlock.js
+++ BondageClub/Screens/Inventory/ItemMisc/FamilyPadlock/FamilyPadlock.js
@@ -2,23 +2,17 @@
 
 /** @type {ExtendedItemCallbacks.Load} */
 function InventoryItemMiscFamilyPadlockLoad() {
+	InventoryItemMiscOwnerPadlockLoad();
 }
 
 /** @type {ExtendedItemCallbacks.Draw} */
 function InventoryItemMiscFamilyPadlockDraw() {
-	DrawAssetPreview(1387, 225, DialogFocusItem.Asset);
-	DrawText(DialogFindPlayer(DialogFocusItem.Asset.Group.Name + DialogFocusItem.Asset.Name + "Intro"), 1500, 600, "white", "gray");
-	if ((DialogFocusSourceItem != null) && (DialogFocusSourceItem.Property != null) && (DialogFocusSourceItem.Property.LockMemberNumber != null))
-		DrawText(DialogFindPlayer("LockMemberNumber") + " " + DialogFocusSourceItem.Property.LockMemberNumber.toString(), 1500, 700, "white", "gray");
-	let msg = DialogFindPlayer(DialogFocusItem.Asset.Group.Name + DialogFocusItem.Asset.Name + "Detail");
-	const subst = ChatRoomPronounSubstitutions(CurrentCharacter, "TargetPronoun", false);
-	msg = CommonStringSubstitute(msg, subst);
-	DrawText(msg, 1500, 800, "white", "gray");
+	InventoryItemMiscOwnerPadlockDraw();
 	if (LogQuery("BlockFamilyKey", "OwnerRule"))
 		DrawText(DialogFindPlayer("ItemMiscFamilyPadlockDetailNoKey"), 1500, 900, "pink", "gray");
 }
 
 /** @type {ExtendedItemCallbacks.Click} */
 function InventoryItemMiscFamilyPadlockClick() {
-	if ((MouseX >= 1885) && (MouseX <= 1975) && (MouseY >= 25) && (MouseY <= 110)) DialogFocusItem = null;
+	InventoryItemMiscOwnerPadlockClick();
 }
