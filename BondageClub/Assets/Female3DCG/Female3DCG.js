b7af613e3a2216cacba5ac2af46c7309baabbeba
bananarama92 <bananarama921@outlook.com>
2023-06-08 15:45:10 +0200

    ENH: Add the new "Bug Legs" item by Toshiro

--- BondageClub/Assets/Female3DCG/Female3DCG.js
+++ BondageClub/Assets/Female3DCG/Female3DCG.js
@@ -461,6 +461,13 @@ var AssetFemale3DCG = [
 					{ Name: "Rneck", HasType: false, CopyLayerColor: "Rfhead", AllowModuleTypes: ["n1"] }
 				]
 			},
+			{
+				Name: "BugLegs", Priority: 6, Value: 40, DefaultColor: ["#406A50", "#7F795F"],
+				Layer: [
+					{ Name: "Legs" },
+					{ Name: "Joints", Priority: 5 },
+				]
+			},
 			// Flat Chest Compatible Clothing
 		],
 		Color: ["Default", "#202020", "#808080", "#bbbbbb", "#aa8080", "#80aa80", "#8080aa", "#aaaa80", "#80aaaa", "#aa80aa", "#cc3333", "#33cc33", "#3333cc", "#cccc33", "#33cccc", "#cc33cc"]
