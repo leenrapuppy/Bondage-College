f9bc2b7044c74b93fa548b1f3fc3922dd4a8c5bf
bananarama92 <bananarama921@outlook.com>
2023-06-11 13:08:38 +0200

    BUG: Add a missing original function call for the lovers vibrator draw function

--- BondageClub/Screens/Inventory/ItemVulva/LoversVibrator/LoversVibrator.js
+++ BondageClub/Screens/Inventory/ItemVulva/LoversVibrator/LoversVibrator.js
@@ -2,6 +2,7 @@
 
 /** @type {ExtendedItemScriptHookCallbacks.Draw<VibratingItemData>} */
 function InventoryItemVulvaLoversVibratorDrawHook(Data, OriginalFunction) {
+	OriginalFunction();
 	const { Asset, Property } = DialogFocusItem;
 	const ItemMemberNumber = DialogFindPlayer("ItemMemberNumber").replace("Item", Asset.Description);
 	DrawText(ItemMemberNumber + " " + Property.ItemMemberNumber, 1500, 450, "white", "gray");
